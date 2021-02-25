ba1029acec3ed64d1e8340c9ec4ca24d31564c4a
subashniraula6@gmail.com <subashniraula6@gmail.com>
2021-02-25 15:23:35 +0545

    sass loader

---
+++ 4_Loaders/SASS Loader/webpack.config.js
@@ -0,0 +1,39 @@
+const path = require('path');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, './dist'),
+        publicPath: 'dist/'
+    },
+    mode: 'none',
+    module: {
+        rules: [
+            // {
+            //     test: /\.(xml)$/,
+            //     use: [
+            //         'xml-loader'
+            //     ]
+            // },
+            {
+                test: /\.(png|jpg)$/,
+                use: [
+                    'file-loader'
+                ]
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    'style-loader', 'css-loader'
+                ]
+            },
+            {
+                test: /\.scss$/,
+                use: [
+                    'style-loader', 'css-loader', 'sass-loader'
+                ]
+            }
+        ]
+    }
+}
