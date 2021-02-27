63145259c099e0b0d2037d5bc94a5155e3865b28
subashniraula6@gmail.com <subashniraula6@gmail.com>
2021-02-27 17:02:12 +0545

    automatic generating html file

---
+++ 5_Plugins/HTML webpack plujgin/webpack.config.js
@@ -0,0 +1,52 @@
+const path = require('path');
+const TerserPlugin = require('terser-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        filename: 'bundle.[contenthash].js',
+        path: path.resolve(__dirname, './dist'),
+        publicPath: ''
+    },
+    mode: 'none',
+    module: {
+        rules: [
+            // {
+            //     test: /\.(xml)$/,
+            //     use: [
+            //         'xml-loader'
+            //     ]
+            // },
+            {
+                test: /\.(png|jpg)$/,
+                use: [
+                    'file-loader'
+                ]
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    // 'style-loader', 'css-loader'
+                    MiniCssExtractPlugin.loader, 'css-loader'
+                ]
+            },
+            {
+                test: /\.scss$/,
+                use: [
+                    MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'
+                ]
+            }
+        ]
+    },
+    plugins: [
+        new TerserPlugin(),
+        new MiniCssExtractPlugin({
+            filename: 'styles.[contenthash].css',
+        }),
+        new CleanWebpackPlugin(),
+        new HtmlWebpackPlugin()
+    ]
+}
