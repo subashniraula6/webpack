81ac10e323cb06722d34ee8f161bfa3826fb21d2
subashniraula6@gmail.com <subashniraula6@gmail.com>
2021-02-27 17:31:03 +0545

    customizing generated html file

--- 5_Plugins/HTML webpack plujgin/webpack.config.js
+++ 5_Plugins/HTML webpack plujgin/webpack.config.js
@@ -47,6 +47,12 @@ module.exports = {
             filename: 'styles.[contenthash].css',
         }),
         new CleanWebpackPlugin(),
-        new HtmlWebpackPlugin()
+        new HtmlWebpackPlugin({
+            title: 'newTitle',
+            filename: 'subfolder/custom_filename.html',
+            meta: {
+                description: "some description"
+            }
+        })
     ]
 }
